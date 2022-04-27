d98f33e71490bae0cdb86f31f1f980d8067d4442
MengxingLiu <mengluo1990@gmail.com>
2022-04-27 17:42:33 +0200

    update

--- s2_organise_data.py
+++ s2_organise_data.py
@@ -1,8 +1,11 @@
+import os
+os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
-import scipy,random, matplotlib, itertools, glob, os, platform, getpass
+import random, matplotlib, itertools, glob, platform, getpass
+import scipy.stats
 import numpy as np
 import matplotlib.gridspec as gridspec
 from pathlib import Path
@@ -50,13 +53,34 @@ noise = noise.reset_index()
 noise = noise.rename(columns = {"T01": "noise_T01", "T02": "noise_T02"})
 noise.to_csv(raw_csv_dir / "noise_clean.csv", index=False)
 
+# tckstats organize
+tckstats = pd.read_csv(raw_csv_dir / 
+                "tckstats_AL_07.csv")
+
+tckstats["stats"] = tckstats.stats.map(lambda x:x.lstrip("b'").rstrip(r"\\n'"))
+tckstats[["tck_mean", "tck_std", "tck_min", "tck_max", "tck_count"]] = (
+            tckstats["stats"].str.split(' ',4,  expand=True)
+)
+tckstats = tckstats.replace({"TCK": tract_dic})
+tckstats.to_csv(git_dir / 
+            "tckstats_AL_07.csv", index=False)
+
 ## concatenate pairwise agreement of test-retest and only AL06vsAL07 to compaire
 ## with noise and other stuff
-pairwise = pairwise[(pairwise["btw"]=="T01vsT02") | 
-                    (pairwise["btw"]=="comAL_06vscomAL_07")]
+pairwise = pd.read_csv(raw_csv_dir / "pairwise_agreement_all_final.csv")
+pairwise = pairwise[["bundle_adjacency_voxels", "dice_voxels", 
+                'density_correlation', 'TCK', 'SUBID', 'btw']]
+# pairwise = pairwise[(pairwise["btw"]=="T01vsT02") | 
+#                     (pairwise["btw"]=="comAL_06vscomAL_07")]
+
 pairwise = pairwise.pivot(index=["SUBID", "TCK"], columns = "btw")
 pairwise.columns = ["_".join(i) for i in pairwise.columns.to_flat_index()]
 pairwise = pairwise.reset_index()
+    
+# calculate computational average
+for i in ["bundle_adjacency_voxels", "dice_voxels", 'density_correlation']:
+    col = pairwise.loc[:,i+"_comAL_01vscomAL_02":i+"_comAL_09vscomAL_10"]
+    pairwise[i+"_comAL_all_average"]=col.mean(axis=1)
 
 # concatenate pairwise and noise
 noise = pd.read_csv(raw_csv_dir / "noise_clean.csv")
@@ -76,16 +100,21 @@ pw_ns_mt = pd.merge(pairwise_noise, head_motion, how = "outer", on = ["SUBID"])
 
 
 # load fa correlation
-fa_corr = pd.read_csv(git_dir / "correlation_fa.csv")
-fa_corr = fa_corr.pivot(index=["SUBID", "TCK"], columns= "btw", values = "corr")
+fa_corr = pd.read_csv(raw_csv_dir / "correlation_all_index.csv")
+fa_corr = fa_corr.pivot(index=["SUBID", "TCK"], columns= "btw", values = "fa_y")
 fa_corr = fa_corr.reset_index()
-fa_corr.columns = ["SUBID", "TCK", "fa_corr_06vs07", "fa_corr_T01vsT02"]
+fa_corr = fa_corr.reindex(sorted(fa_corr.columns), axis=1)
+fa_corr.rename(columns = {i:i+"_fa" for i in fa_corr.columns
+                        if ((i != "SUBID") and (i != "TCK")) }, inplace=True)
+# calculate average across computational correlation
+col = fa_corr.loc[:, "AL_01vsAL_02_fa":"AL_09vsAL_10_fa"]
+fa_corr["AL_all_fa"] = col.mean(axis=1)
 
 # concatenate pairwise, noise, head_motion and fa correlation
 pw_fa_ns_mt = pd.merge(pw_ns_mt, fa_corr, how = "outer", on = ["SUBID", "TCK"])
 
 # load tck stats
-tckstats = pd.read_csv(git_dir / "tckstats_AL_07.csv")
+tckstats = pd.read_csv(raw_csv_dir / "tckstats_AL_07.csv")
 tckstats = tckstats.pivot(index = ["SUBID", "TCK"], columns = "ses")
 tckstats.columns = ["_".join(i) for i in tckstats.columns.to_flat_index()]
 tckstats = tckstats.reset_index()
@@ -95,42 +124,31 @@ pw_fa_ns_mt_st = pd.merge(pw_fa_ns_mt, tckstats,
                         how="outer", on = ["SUBID", "TCK"])
 # calculate noise difference between T01 and T02
 pw_fa_ns_mt_st["noise_diff_T01-T02"] = pw_fa_ns_mt_st["noise_T01"] - pw_fa_ns_mt_st["noise_T02"]
-pw_fa_ns_mt_st.to_csv(git_dir / "pw_fa_ns_mt_st.csv", index=False)
+pw_fa_ns_mt_st.to_csv(raw_csv_dir / "pw_fa_ns_mt_st.csv", index=False)
 
 
-# tckstats organize
-tckstats = pd.read_csv(raw_csv_dir / 
-                "tckstats_AL_07.csv")
-
-tckstats["stats"] = tckstats.stats.map(lambda x:x.lstrip("b'").rstrip(r"\\n'"))
-tckstats[["tck_mean", "tck_std", "tck_min", "tck_max", "tck_count"]] = (
-            tckstats["stats"].str.split(' ',4,  expand=True)
-)
-tckstats = tckstats.replace({"TCK": tract_dic})
-tckstats.to_csv(git_dir / 
-            "tckstats_AL_07.csv", index=False)
-# comparie before and after fix
-tract_to_plot = df[df["analysis"]=="AL_07_fix"].tract.unique()
-fig, axes = plt.subplots()
-sns.stripplot(x = "tract", y = "dice_voxels", 
-                data = df[df["tract"].isin(tract_to_plot)],
-                order = tract_to_plot, 
-                hue = "analysis", ax = axes, alpha=0.5)
-sns.pointplot(x = "tract", y = "dice_voxels", 
-                order = tract_to_plot,
-                data = df[df["tract"].isin(tract_to_plot)],
-                hue = "analysis", alpha = 0.55, ax = axes, join=False)
-plt.show()
 
 
-pairwise_0607 = pd.read_csv(raw_csv_dir / "pairwise_agreement_comAL_06vscomAL_07.csv")
-pairwise = pd.concat([pairwise_TRT, pairwise_0607])
-pairwise = pairwise.replace({"tract":tract_dic})
-pairwise = pairwise.rename(columns={"tract":"TCK"})
 
-# calculate description
-pairwise.groupby(["btw", "TCK"]).describe().to_csv("pairwise_description.csv")
 
+pairwise = pd.read_csv(raw_csv_dir / "pairwise_agreement_all_final.csv")
+# calculate description for pairwise agreement TRT
+inds = ["bundle_adjacency_voxels", "dice_voxels", 'density_correlation']
+pairwise[pairwise["btw"]=="T01vsT02"].groupby(["TCK"]
+            )[inds].describe().to_csv(raw_csv_dir / "pairwise_description_TRT.csv")
+# calculate description for pairwise agreement computational
+pairwise[~(pairwise["btw"]=="T01vsT02")].groupby(["TCK"]
+            )[inds].describe(
+            ).to_csv(raw_csv_dir / "pairwise_description_computation.csv")
+
+# calculate description for FA correlation
+fa_corr = pd.read_csv(raw_csv_dir / "correlation_all_index.csv")
+fa_corr[fa_corr["btw"]=="test-retest_AL_07"].groupby(["TCK"]
+            ).describe(
+            )["fa_y"].to_csv(raw_csv_dir / "correlation_description_TRT.csv")
+fa_corr[~(fa_corr["btw"]=="test-retest_AL_07")].groupby(["TCK"]
+            ).describe(
+            )["fa_y"].to_csv(raw_csv_dir / "correlation_description_computation.csv")
 
 # organize noise file
 noise = pd.read_csv(raw_csv_dir / "noise.csv")
